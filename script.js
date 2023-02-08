3088cf4f123a86b86dfa35a18a9ab6a35e350ee1
Ginicoefficient <104734969+Ginicoefficient@users.noreply.github.com>
2023-02-08 13:59:26 -0600

    finished solo project if return change responsiveness

--- script.js
+++ script.js
@@ -1,34 +1,40 @@
 const placeholderHtml = `<div class = "single-color-holder">
-<div class="color-img">TEST</div>
-<div class="color-hex">TEST</div>
+<div class="color-img" style="background-color: #d5d4d8">TEST</div>
+<div class="color-hex"></div>
 </div>`;
 let colorsArray = new Array(5).fill(placeholderHtml);
 
 const render = () => {
-  const colorsHtml = colorsArray
-    .map((color) => {
-      return `<div class = "single-color-holder" id ="">
+  if (document.getElementById("scheme-selector").value == "") {
+    document.getElementById("color-holder").innerHTML = colorsArray.join("");
+  } else {
+    const colorsHtml = colorsArray
+      .map((color) => {
+        return `<div class = "single-color-holder" id ="">
             <div class="color-img" style="background-color: ${color.hex.value};"></div>
-            <div class="color-hex">${color.hex.value}</div>
+            <div class="color-hex"><p>${color.hex.value}</p></div>
         </div>
         `;
-    })
-    .join("");
-  document.getElementById("color-holder").innerHTML = colorsHtml;
+      })
+      .join("");
+    document.getElementById("color-holder").innerHTML = colorsHtml;
+  }
 };
 
-const submitRequest = () => {
+function submitRequest() {
   const color = document.getElementById("color-picker").value.slice(1);
   const schemeChoice = document.getElementById("scheme-selector").value;
   const fetchString = `https://www.thecolorapi.com/scheme?hex=${color}&mode=${schemeChoice}&count=5`;
-
-  fetch(fetchString)
-    .then((res) => res.json())
-    .then((data) => {
-      colorsArray = data.colors;
-      console.log(colorsArray);
-      render();
-    });
-};
+  if (!document.getElementById("scheme-selector").value == "") {
+    fetch(fetchString)
+      .then((res) => res.json())
+      .then((data) => {
+        colorsArray = data.colors;
+        console.log(colorsArray);
+        render();
+      });
+  }
+}
+render();
 
 document.getElementById("submit-btn").addEventListener("click", submitRequest);
